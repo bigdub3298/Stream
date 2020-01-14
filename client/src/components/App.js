804d1923ccf20ecc3cd2df4eecdfdafd4549f56e
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-14 15:16:18 -0700

    set up application routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamCreate from "./streams/StreamCreate";
+import StreamEdit from "./streams/StreamEdit";
+import StreamDelete from "./streams/StreamDelete";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
 
 export default function App() {
-  return <div>App of the year</div>;
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/stream/new" exact component={StreamCreate} />
+          <Route path="/stream/delete" exact component={StreamDelete} />
+          <Route path="/stream/edit" exact component={StreamEdit} />
+          <Route path="/stream/show" exact component={StreamShow} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
