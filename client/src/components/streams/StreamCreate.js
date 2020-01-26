b0a8471023e32dbf630ff1c34da1515536ed2c64
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-26 00:48:46 -0700

    hooked up redux-form and created streamcreate form

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,60 @@
-import React from "react";
+import React, { Component } from "react";
+import { Field, reduxForm, clearSubmitErrors } from "redux-form";
 
-export default function StreamCreate() {
-  return <div>StreamCreate</div>;
+export class StreamCreate extends Component {
+  renderField({ input, label, type }) {
+    return (
+      <div className="row">
+        <div className="field ten column wide">
+          <label>{label}</label>
+          <div>
+            <input {...input} placeholder={label} type={type} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form centered grid column"
+      >
+        <Field
+          name="title"
+          type="text"
+          component={this.renderField}
+          label="Title"
+        />
+        <Field
+          name="description"
+          type="text"
+          component={this.renderField}
+          label="Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
 }
+
+const validate = values => {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "Must enter a title";
+  }
+
+  if (!values.description) {
+    errors.description = "Must enter a description";
+  }
+};
+
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
