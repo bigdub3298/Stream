c0cf9961320b58ec841e6984ca9817f6fdba35c0
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-26 17:57:57 -0700

    created all action creaters for stream manipualtion

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -15,3 +15,27 @@ export const createStream = values => async dispatch => {
   const response = await streams.post("/streams", values);
   dispatch({ type: "CREATE_STREAM", payload: response.data });
 };
+
+export const fetchAllStreams = () => async dispatch => {
+  const response = await streams.get("/streams");
+
+  dispatch({ type: "FETCH_STREAMS", payload: response.data });
+};
+
+export const fetchStream = id => async dispatch => {
+  const response = await streams.get(`/streams/${id}`);
+
+  dispatch({ type: "FETCH_STREAM", payload: response.data });
+};
+
+export const editStream = (id, values) => async dispatch => {
+  const response = await streams.put(`/streams/${id}`, values);
+
+  dispatch({ type: "EDIT_STREAM", payload: response.data });
+};
+
+export const deleteStream = id => async dispatch => {
+  await streams.delete(`/streams/${id}`);
+
+  dispatch({ type: "DELETE_STREAM", payload: id });
+};
