6be7ffab9eb0e5e9edd7ef4cbba95b3901d4e2a3
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-26 21:41:48 -0700

    implemented admin edit/delete buttons on current user streams

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,15 +1,34 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchAllStreams } from "../../actions";
+import { Link } from "react-router-dom";
 
 class StreamList extends Component {
+  renederAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <Link to="/stream/edit" className="ui button primary">
+            Edit
+          </Link>
+          <Link to="/stream/delete" className="ui button negative">
+            Delete
+          </Link>
+        </div>
+      );
+    }
+  }
+
   renderStreamList() {
     return this.props.streams.map(stream => {
       return (
         <div key={stream.id} className="item">
+          {this.renederAdmin(stream)}
           <i className="large middle aligned icon camera" />
           <div className="content">
-            <div className="header">{stream.title}</div>
+            <Link to="/stream/show" className="header">
+              {stream.title}
+            </Link>
             <div className="description">{stream.description}</div>
           </div>
         </div>
@@ -32,7 +51,10 @@ class StreamList extends Component {
 }
 
 const mapStateToProps = state => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
+  };
 };
 
 export default connect(mapStateToProps, {
