e43487dc5119b6997863ee033ece9b319d67f627
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-28 16:17:42 -0700

    factored out duplicate code and create reusable component

---
+++ client/src/components/streams/streamForm.js
@@ -0,0 +1,79 @@
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+
+export class StreamForm extends Component {
+  renderError(error, touched) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  }
+
+  renderField = ({ input, label, type, meta: { touched, error } }) => {
+    return (
+      <div className="row">
+        <div className="field ten column wide">
+          <label>{label}</label>
+          <div>
+            <input
+              {...input}
+              placeholder={label}
+              type={type}
+              autoComplete="off"
+            />
+            {this.renderError(error, touched)}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  onSubmit = values => {
+    this.props.onSubmit(values);
+  };
+
+  render() {
+    return (
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form centered grid column error"
+      >
+        <Field
+          name="title"
+          type="text"
+          component={this.renderField}
+          label="Title"
+        />
+        <Field
+          name="description"
+          type="text"
+          component={this.renderField}
+          label="Description"
+        />
+        <button className="ui button primary">Submit</button>
+      </form>
+    );
+  }
+}
+
+const validate = values => {
+  const errors = {};
+
+  if (!values.title) {
+    errors.title = "You must enter a title";
+  }
+
+  if (!values.description) {
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
+};
+
+export default reduxForm({
+  form: "streamForm",
+  validate
+})(StreamForm);
