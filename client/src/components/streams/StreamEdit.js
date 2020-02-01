b95374703b01ad531cb9726a9721486d61e3802f
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-31 19:14:19 -0700

    refactored loading screen to take up entire view port

--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -15,10 +15,8 @@ class StreamEdit extends Component {
   render() {
     if (!this.props.stream) {
       return (
-        <div className="ui segment">
-          <div className="ui active inverted dimmer">
-            <div className="ui text loader">Loading...</div>
-          </div>
+        <div className="ui active inverted dimmer">
+          <div className="ui text loader">Loading...</div>
         </div>
       );
     }
