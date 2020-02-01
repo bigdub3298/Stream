a16b16958f2f677592983c9460b5756e1afd80e0
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-31 19:14:45 -0700

    setup stream details

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
-export default function StreamShow() {
-  return <div>StreamShow</div>;
+class StreamShow extends Component {
+  componentDidMount() {
+    const { id } = this.props.match.params;
+    this.props.fetchStream(id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return (
+        <div className="ui active inverted dimmer">
+          <div className="ui text loader">Loading...</div>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <h1>{this.props.stream.title}</h1>
+        <p>{this.props.stream.description}</p>
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = (state, ownProps) => {
+  const { id } = ownProps.match.params;
+  return { stream: state.streams[id] };
+};
+export default connect(mapStateToProps, {
+  fetchStream
+})(StreamShow);
